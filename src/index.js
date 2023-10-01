aa663db473743400b8a36b57ae31386857dc070f
Ivan Rudenko <rudenko.ivan81@gmail.com>
2023-10-01 17:14:09 +0200

    save

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
 
-// Funkcja do aktualizacji select.breed-select
+
 function updateBreedSelect(breeds) {
   breedSelect.innerHTML = "";
   breeds.forEach((breed) => {
@@ -16,9 +16,9 @@ function updateBreedSelect(breeds) {
   });
 }
 
-// Funkcja do aktualizacji catInfo
+
 function updateCatInfo(catData) {
-  const cat = catData[0]; // Pobieramy pierwszego kota z wyniku
+  const cat = catData[0]; 
   catInfo.innerHTML = `
     <h2>${cat.breeds[0].name}</h2>
     <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
@@ -27,34 +27,34 @@ function updateCatInfo(catData) {
   `;
 }
 
-// Obsługa zmiany wybranej rasy
+
 breedSelect.addEventListener("change", () => {
   const selectedBreedId = breedSelect.value;
   
-  // Ukrywamy catInfo i pokazujemy loader podczas pobierania informacji o kocie
+
   catInfo.style.display = "none";
   loader.style.display = "block";
 
   fetchCatByBreed(selectedBreedId)
     .then((catData) => {
-      // Po pobraniu informacji o kocie aktualizujemy catInfo i ukrywamy loader
+
       updateCatInfo(catData);
       loader.style.display = "none";
       catInfo.style.display = "block";
     })
     .catch((error) => {
-      // W przypadku błędu wyświetlamy komunikat błędu i ukrywamy loader
+
       error.style.display = "block";
       loader.style.display = "none";
     });
 });
 
-// Inicjalizacja - pobieramy i wypełniamy select.breed-select
+
 fetchBreeds()
   .then((breeds) => {
     updateBreedSelect(breeds);
   })
   .catch((error) => {
-    // W przypadku błędu wyświetlamy komunikat błędu
+
     error.style.display = "block";
   });
